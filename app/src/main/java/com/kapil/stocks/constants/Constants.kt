9fc395b60d1008f410ef4f5d1e47d8c56c2673dd
Kapil Kumar <kapilmaan0123@gmail.com>
2025-07-01 18:41:20 +0530

    high level watch list support added

--- app/src/main/java/com/kapil/stocks/constants/Constants.kt
+++ app/src/main/java/com/kapil/stocks/constants/Constants.kt
@@ -1,7 +1,10 @@
 package com.kapil.stocks.constants;
 
+
 object Constants {
     const val TAG = "MyStocks"
     const val COMPANY_NAME = "companyName"
     const val LIST_TYPE = "listType"
+    const val WATCHLIST_PREF_FILE = "watchlist"
+    const val WATCHLIST_DATA = "watchlistData"
 }
